a580ed9ae923b137a311313880c1581d39935c76
akatsuki <66823178+i-akatsuki@users.noreply.github.com>
2026-03-04 08:20:07 +0800

    will apply changes here from now on.

--- pos_project/config/settings/prod.py
+++ pos_project/config/settings/prod.py
@@ -17,7 +17,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ] + MIDDLEWARE[1:]  # noqa: F405
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.  '
 
 # Stricter security headers for production
 SECURE_BROWSER_XSS_FILTER = True
